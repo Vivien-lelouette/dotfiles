293bdadbfe19ad22d659bd3c3bc9ebed7729a8b3
Vivien LELOUETTE <vivien.lelouette@ama-lt-4553.local>
2021-01-26 09:03:52 +0100

    Fix a stow bug with polybar/config view as a directory

--- emacs/.config/polybar/start.sh
+++ emacs/.config/polybar/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload -c ~/.config/polybar/config panel &
+    MONITOR=$m polybar --reload -c ~/.config/polybar/config.txt panel &
   done
 else
-  polybar --reload -c ~/.config/polybar/config panel &
+  polybar --reload -c ~/.config/polybar/config.txt panel &
 fi
