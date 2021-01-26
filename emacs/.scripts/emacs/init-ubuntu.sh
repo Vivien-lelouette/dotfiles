11a21306cf5c2bb86c623a8505169aaa4f20539f
Vivien LELOUETTE <vivien.lelouette@ama-lt-4553.local>
2021-01-26 09:02:41 +0100

    Fix fonts installation

--- emacs/.scripts/emacs/init-ubuntu.sh
+++ emacs/.scripts/emacs/init-ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt install autoconf make gcc texinfo libgtk-3-dev libxpm-dev libjpeg-dev libgif-dev libtiff5-dev libgnutls28-dev libncurses5-dev
 
-sudo apt install fonts-firacode font-cantarell
+sudo apt install fonts-firacode fonts-cantarell
 
 sudo apt install cmake libtool libtool-bin zsh
