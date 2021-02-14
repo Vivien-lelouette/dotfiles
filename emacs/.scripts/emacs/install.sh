99e339d6b2bff241cc50cd331b7d929ef6fe0b3d
Vivien Lelouette <lelouette.vivien@gmail.com>
2021-02-14 22:49:29 +0100

    Improve emacs build setup.

--- emacs/.scripts/emacs/install.sh
+++ emacs/.scripts/emacs/install.sh
@@ -4,5 +4,5 @@ cd ~/Tools
 git clone git://git.savannah.gnu.org/emacs.git -b feature/native-comp
 cd emacs
 ./autogen.sh
-./configure --with-nativecomp
-make -j$(nproc)
+./configure --with-nativecomp --with-modules --with-xwidgets --with-cairo --with-mailutils --with-jansson
+make -j 8
