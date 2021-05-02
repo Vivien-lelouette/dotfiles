e00cfe17b59123ab0fca31971e9bd6bd26c816ca
Vivien Lelouette <lelouette.vivien@gmail.com>
2021-05-02 16:52:11 +0200

    Going back to the git master as nativcomp is merged

--- emacs/.scripts/emacs/install.sh
+++ emacs/.scripts/emacs/install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 mkdir -p ~/Tools/emacs-pgtk-nativ-comp
-# cd ~/Tools
-# git clone git://git.savannah.gnu.org/emacs.git -b feature/native-comp
-cd ~/Tools/emacs-pgtk-nativ-comp
+cd ~/Tools
+git clone git://git.savannah.gnu.org/emacs.git
 cd emacs
 export CC=/usr/bin/gcc-10 CXX=/usr/bin/gcc-10
 ./autogen.sh
+# Will add --with-pgtk when merged to master
 ./configure --with-dbus --with-gif --with-jpeg --with-png --with-rsvg \
          --with-tiff --with-xft --with-xpm --with-gpm=no \
-         --with-modules --with-native-compilation --with-pgtk --with-cairo
+         --with-modules --with-native-compilation --with-cairo
 make -j 8 NATIVE_FULL_AOT=1
 sudo make install
