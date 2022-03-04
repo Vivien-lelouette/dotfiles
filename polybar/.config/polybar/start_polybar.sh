13c1ed4cd77aa3a1bd293b794811953812d4eb49
Vivien Lelouette <lelouette.vivien@gmail.com>
2022-03-04 10:19:00 +0100

    polybar: window title max length set by monitor

--- polybar/.config/polybar/start_polybar.sh
+++ polybar/.config/polybar/start_polybar.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 pkill polybar
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-    echo "$m"
-    MONITOR=$m polybar --quiet --reload hlwm &
+for m in $(polybar --list-monitors | sed 's/ //g'); do
+    curr_monitor=$(echo "$m" | cut -d":" -f1)
+    max_title_width=$(($[$(echo "$m" | cut -d":" -f2 | cut -d"x" -f1)-665] / 9))
+    MONITOR=$curr_monitor MAX_TITLE_WIDTH=$max_title_width polybar --quiet --reload hlwm &
 done
