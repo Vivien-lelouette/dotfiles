46d1c09c67c5c844bd89ef0d81453c70a70f6537
Vivien Lelouette <lelouette.vivien@gmail.com>
2022-03-26 12:32:26 +0100

    easy folding thanks to Karthinks tip !

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -97,6 +97,48 @@
   (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
   (ace-window-display-mode 1))
 
+(use-package hideshow
+  :hook
+  (prog-mode . hs-minor-mode)
+  :bind
+      :bind (
+             ("C-<tab>" . hs-cycle)
+             ("C-<iso-lefttab>" . hs-global-cycle))
+  :config
+  (defun hs-cycle (&optional level)
+    (interactive "p")
+    (let (message-log-max
+          (inhibit-message t))
+      (if (= level 1)
+          (pcase last-command
+            ('hs-cycle
+             (hs-hide-level 1)
+             (setq this-command 'hs-cycle-children))
+            ('hs-cycle-children
+             ;; TODO: Fix this case. `hs-show-block' needs to be
+             ;; called twice to open all folds of the parent
+             ;; block.
+             (save-excursion (hs-show-block))
+             (hs-show-block)
+             (setq this-command 'hs-cycle-subtree))
+            ('hs-cycle-subtree
+             (hs-hide-block))
+            (_
+             (if (not (hs-already-hidden-p))
+                 (hs-hide-block)
+               (hs-hide-level 1)
+               (setq this-command 'hs-cycle-children))))
+        (hs-hide-level level)
+        (setq this-command 'hs-hide-level))))
+
+  (defun hs-global-cycle ()
+    (interactive)
+    (pcase last-command
+      ('hs-global-cycle
+       (save-excursion (hs-show-all))
+       (setq this-command 'hs-global-show))
+      (_ (hs-hide-all)))))
+
 (use-package ediff
     :straight (:type built-in)
     :custom
