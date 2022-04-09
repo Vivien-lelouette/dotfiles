e32e00bb0b8678f5d2fdc782701c781add472cf8
Vivien Lelouette <lelouette.vivien@gmail.com>
2022-04-09 09:12:30 +0200

    compile emacs with json support

--- emacs/.scripts/emacs/install.sh
+++ emacs/.scripts/emacs/install.sh
@@ -5,6 +5,6 @@ git clone --branch emacs-28 git://git.savannah.gnu.org/emacs.git
 cd emacs
 export CC=/usr/bin/gcc-10 CXX=/usr/bin/gcc-10
 ./autogen.sh
-./configure --with-dbus --with-gif --with-jpeg --with-png --with-rsvg --with-x-toolkit=gtk --with-xwidgets  --with-cairo --with-mailutils -without-pop --with-native-compilation
+./configure --with-dbus --with-gif --with-jpeg --with-png --with-rsvg --with-x-toolkit=gtk --with-xwidgets --with-cairo --with-mailutils -without-pop --with-json --with-native-compilation
 make -j 8 NATIVE_FULL_AOT=1
 sudo make install
