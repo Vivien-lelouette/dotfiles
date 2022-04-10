514133114a3849b4d51d2930958aa0a876ac2a60
Vivien Lelouette <lelouette.vivien@gmail.com>
2022-04-10 21:30:59 +0200

    Adding keybind to balance-windows

--- emacs/.emacs.d/desktop.el
+++ emacs/.emacs.d/desktop.el
@@ -241,6 +241,7 @@
           ([?\s-&] . shrink-window)
           ([?\s-{] . shrink-window-horizontally)
           ([?\s-}] . enlarge-window-horizontally)
+          ([?\s-=] . balance-windows)
 
           ([?\s-k] . kill-current-buffer)
           ([?\s-q] . exwm/kill-current-buffer-and-window)
